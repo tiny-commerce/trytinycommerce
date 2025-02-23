71a7cd67a1b2caa37f011636ca0ea053b9bcdc5f
Josh Evensen <joshevensen@outlook.com>
2025-02-22 23:42:01 -0600

    working on faq

---
+++ components/content/FaqItem.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import {MinusSmallIcon, PlusSmallIcon} from "@heroicons/vue/24/outline";
+import {Disclosure, DisclosureButton, DisclosurePanel} from "@headlessui/vue";
+</script>
+
+<template>
+  <div class="mx-auto max-w-2xl">
+  <Disclosure v-slot="{ open }" as="div" class="py-6 first:pt-0 last:pb-0">
+    <dt>
+      <DisclosureButton class="flex w-full items-start justify-between text-left text-gray-900">
+        <span class="text-xl/7 font-semibold">
+          <slot/>
+        </span>
+        <span class="ml-6 flex h-7 items-center">
+          <PlusSmallIcon v-if="!open" aria-hidden="true" class="size-6"/>
+          <MinusSmallIcon v-else aria-hidden="true" class="size-6"/>
+        </span>
+      </DisclosureButton>
+    </dt>
+    <DisclosurePanel as="dd" class="mt-2 pr-12">
+      <p class="text-base/7 text-gray-600">
+        <slot name="answer"/>
+      </p>
+    </DisclosurePanel>
+  </Disclosure>
+  </div>
+</template>
--- content/pages/faq.md
+++ content/pages/faq.md
@@ -10,5 +10,9 @@ You've got questions. We have answers.
 Have a different question and can’t find the answer you’re looking for? :nuxt-link[Contact Us]{.font-semibold.text-primary.hover:underline to="/contact"}
 ::
 
-::faq-list
+::faq-item
+Can I Cancel At Anytime?
+
+#answer
+<strong>Yep</strong>. But why would you want to?
 ::
